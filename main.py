49655744891e59b01b4e09d0a3ff92dc8fd5e5ce
syauqikhosyi <75014712+syauqikhosyi@users.noreply.github.com>
2024-12-27 18:46:18 +0700

    Update main.py
    Create Rupiah's function and integer format

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Run it with 'streamlit run main.py
+# Run it with 'streamlit run main.py'
 
 # Import streamlit
 import streamlit as st
@@ -8,7 +8,8 @@ st.set_page_config(
     page_title="Oci & Mine's Jastip Calculator",
     page_icon="🧮",
     layout="wide",
-    initial_sidebar_state="expanded")
+    initial_sidebar_state="expanded"
+)
 
 # Initialize the Streamlit app
 st.title("Oci & Mine's Jastip Calculator")
@@ -22,25 +23,28 @@ fullTransportFee = 40000
 transportFee = fullTransportFee / 5
 
 fullMealFee = 150000
-mealFee = fullMealFee/ 5
+mealFee = fullMealFee / 5
+
+# Function to format numbers as Rupiah
+def format_rupiah(value):
+    return f"Rp {value:,.0f}".replace(",", ".")
 
 # The form
 with st.form("my_calculator"):
     st.subheader("Enter the Product Description")
     myProductName = st.text_input("Product Name")
-    myProductAmount = st.number_input('Product Amount')
-    myJastipFeePercentage = st.number_input('Jastip Fee Percentage', min_value=0.1, max_value=0.3)
+    myProductAmount = st.number_input('Product Amount', min_value=0, step=1)
+    myJastipFeePercentage = st.number_input('Jastip Fee Percentage', min_value=0.1, max_value=0.3, step=0.1)
     submit = st.form_submit_button('Calculate!')
-    
+
 if submit:
-    jastipFee = myJastipFeePercentage * myProductAmount
-    result = myProductAmount + jastipFee + packaging + transportFee + mealFee
-    st.write("The total is: Rp", result)
-    st.write("Here's the detail")
-    st.write()
-    st.write("Product Name: ", myProductName)
-    st.write("Product Amount: Rp", myProductAmount)
-    st.write("Jastip Fee: Rp", jastipFee)
-    st.write("Packaging: Rp", packaging)
-    st.write("Transport Fee: Rp", transportFee)
-    st.write("Meal Fee: Rp", mealFee)
+    jastipFee = int(myJastipFeePercentage * myProductAmount)
+    result = int(myProductAmount + jastipFee + packaging + transportFee + mealFee)
+    st.write("### The total is:", format_rupiah(result))
+    st.write("### Here's the detail:")
+    st.write(f"- **Product Name:** {myProductName}")
+    st.write(f"- **Product Amount:** {format_rupiah(int(myProductAmount))}")
+    st.write(f"- **Jastip Fee:** {format_rupiah(jastipFee)}")
+    st.write(f"- **Packaging:** {format_rupiah(packaging)}")
+    st.write(f"- **Transport Fee:** {format_rupiah(int(transportFee))}")
+    st.write(f"- **Meal Fee:** {format_rupiah(int(mealFee))}")
