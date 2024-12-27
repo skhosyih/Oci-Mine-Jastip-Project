1123d75ead788a48f015a43a257b526d450b4d15
syauqikhosyi <75014712+syauqikhosyi@users.noreply.github.com>
2024-12-27 17:25:31 +0700

    Update main.py
    Create the form and the calculation

--- main.py
+++ main.py
@@ -14,5 +14,32 @@ st.set_page_config(
 st.title("Oci & Mine's Jastip Calculator")
 st.write("An interesting entrusted service project initiated by two people, Oci and Mine, that can calculate the entire price of the product along with the 'jastip' rate, transportation costs, packaging, and meal costs, with a fair price")
 
-# Enter the price product amount 
+# Enter the initial price product amount and initial other variables
+priceProduct = 0
 
+percentageJastipFee = 0.1
+jastipFee = percentageJastipFee * priceProduct
+
+packaging = 0
+
+fullTransportFee = 40000
+transportFee = fullTransportFee / 5
+
+fullMealFee = 150000
+mealFee = fullMealFee/ 5
+
+# The form
+with st.form("my_calculator"):
+    st.title("## Enter the Product Amount")
+    myProductAmount = st.number_input('myProductAmount')
+    submit = st.form_submit_button('Calculate!')
+    
+if submit:
+    st.write(f'{myProductAmount + jastipFee + transportFee + mealFee}')
+    st.write("Here's the detail")
+    st.write()
+    st.write("Price Product: ", priceProduct)
+    st.write("Jastip Fee: ", jastipFee)
+    st.write("Packaging: ", packaging)
+    st.write("Transport Fee: ", transportFee)
+    st.write("Meal Fee: ", mealFee)
