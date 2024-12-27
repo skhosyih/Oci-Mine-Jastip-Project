d3ab8e0fc3821d7e7fa4571a06752af5ba248a0a
syauqikhosyi <75014712+syauqikhosyi@users.noreply.github.com>
2024-12-27 16:10:47 +0700

    Update main.py
    Set the page configuration and change component from write() to markdown()

--- main.py
+++ main.py
@@ -4,9 +4,15 @@
 import streamlit as st
 
 # Set the page configuration
-
+st.set_page_config(
+    page_title="Oci & Mine's Jastip Calculator",
+    page_icon="🧮",
+    layout="wide",
+    initial_sidebar_state="expanded")
 
 # Initialize the Streamlit app
 st.title("Oci & Mine's Jastip Calculator")
-st.write("An interesting entrusted service project initiated by two people, Oci and Mine, that can calculate the entire price of the product along with the 'jastip' rate, transportation costs, packaging, and meal costs, with a fair price")
+st.markdown("An interesting entrusted service project initiated by two people, Oci and Mine, that can calculate the entire price of the product along with the 'jastip' rate, transportation costs, packaging, and meal costs, with a fair price")
+
+# Enter the price product amount 
 
