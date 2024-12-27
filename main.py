f499a468f65a57ff8371f25b0d3f624c3dd38d62
syauqikhosyi <75014712+syauqikhosyi@users.noreply.github.com>
2024-12-27 17:29:39 +0700

    Update main.py
    Change the product amount component

--- main.py
+++ main.py
@@ -29,12 +29,12 @@ mealFee = fullMealFee/ 5
 
 # The form
 with st.form("my_calculator"):
-    st.title("## Enter the Product Amount")
+    st.subheader("Enter the Product Amount")
     myProductAmount = st.number_input('myProductAmount')
     submit = st.form_submit_button('Calculate!')
     
 if submit:
-    jastipFee = percentageJastipFee * priceProduct
+    jastipFee = percentageJastipFee * myProductAmount
     st.write(f'{myProductAmount + jastipFee + transportFee + mealFee:.2f}')
     st.write("Here's the detail")
     st.write()
